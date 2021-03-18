af1cb1539f1f5c7dcd97dea6ed8a237741466947
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-18 12:30:25 +0700

    fix sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -17,20 +17,20 @@
                 </li>
                 @if(auth()->user()->role == 'admin')
                 <li class="sidebar-list"><a class="sidebar-link sidebar-title link-nav" href="{{ route('home') }}"><i data-feather="home"> </i><span>Dashboard</span></a></li>
-                <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="user"></i><span>Kelola Akun</span></a>
+                <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="user"></i><span>Kelola User</span></a>
                     <ul class="sidebar-submenu">
                         <li><a href="{{ route('admin.account.register.index') }}">Daftar Akun</a></li>
-                        <li><a href="{{ route('admin.account.admin') }}">Admin Akun</a></li>
-                        <li><a href="{{ route('admin.account.customer') }}">Customer Akun</a></li>
+                        <li><a href="{{ route('admin.account.admin') }}">Admin</a></li>
+                        <li><a href="{{ route('admin.account.customer') }}">User</a></li>
                     </ul>
                 </li>
-                <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="book-open"></i><span>Rekap Data KRP</span></a>
+                <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="book-open"></i><span>Rekap Data KPR</span></a>
                     <ul class="sidebar-submenu">
                         <li><a href="{{ route('admin.rekapdata.bulan') }}">Perbulan</a></li>
                         <li><a href="{{ route('admin.rekapdata.tahun') }}">Pertahun</a></li>
                     </ul>
                 </li>
-                <li class="sidebar-list"><a class="sidebar-link sidebar-title active" href="#"><i data-feather="book"></i><span">Detail Data Angsuran</span>
+                <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="book"></i><span>Detail Data Angsuran</span>
                     </a>
                     <ul class="sidebar-submenu">
                         <li><a href="{{ route('admin.detaildata.angsuranke') }}">Angsuran Ke</a></li>
